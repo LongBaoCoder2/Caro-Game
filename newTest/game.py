d7961982ce01ab8a671bd7b029fd57100df541ea
tachithanhdanh-desktop <22120049@student.hcmus.edu.vn>
2022-12-24 16:44:34 +0700

    fix synchronization bug in setting.json by moving global data into constructor

--- newTest/game.py
+++ newTest/game.py
@@ -1,37 +1,38 @@
 import pygame, json, sys, winlose
-from lib.play_sound import PlaySound
+#from lib.play_sound import PlaySound
 from lib import color, save_manager, win_checker
 from lib import text_switcher, cursor_trail, bot
 from subwindow import *
-
-theme_color = json.load(open('themes/theme.json'))
-setting = json.load(open('data/setting.json'))
-# screen
-SCREEN_WIDTH  = setting['screen']['width']
-SCREEN_HEIGHT = setting['screen']['height']
-# grid
-# NUM_OF_LINES  = setting['grid']['num_of_lines']
-SIZE_X        = min(setting['grid']['size_x'], 100)
-SIZE_Y        = min(setting['grid']['size_y'], 100)
-THICKNESS     = setting['grid']['thickness']
-# GRID_COLOR    = ["#5D5FEF", "#843CE0"]
-# GRID_COLOR = [setting['grid']['color_0'], setting['grid']['color_1']]
-BACKGROUND_COLOR = "#501be8"
-LINE_COLOR       = color.BLACK
-# theme
-THEME = setting['theme']
 
 class Game:
 
     # khởi tạo
     def __init__(self, screen):
+        
+        theme_color = json.load(open('themes/theme.json'))
+        self.setting = json.load(open('data/setting.json'))
+        # screen
+        self.SCREEN_WIDTH  = self.setting['screen']['width']
+        self.SCREEN_HEIGHT = self.setting['screen']['height']
+        # grid
+        # NUM_OF_LINES  = setting['grid']['num_of_lines']
+        self.SIZE_X        = min(self.setting['grid']['size_x'], 100)
+        self.SIZE_Y        = min(self.setting['grid']['size_y'], 100)
+        self.THICKNESS     = self.setting['grid']['thickness']
+        # GRID_COLOR    = ["#5D5FEF", "#843CE0"]
+        # GRID_COLOR = [setting['grid']['color_0'], setting['grid']['color_1']]
+        self.BACKGROUND_COLOR = "#501be8"
+        self.LINE_COLOR       = color.BLACK
+        # theme
+        self.THEME = self.setting['theme']
+
 
         # pygame clock
         self.clock = pygame.time.Clock()
 
         # ảnh lưới và cờ
-        self.img_grid  = pygame.transform.scale(pygame.image.load('res/images/' + THEME + '/grid.png'), (64, 64))
-        self.img_piece = [pygame.transform.scale(pygame.image.load('res/images/' + THEME + '/piece_' + str(i) + '.png'), (32, 32)) for i in range(2)]
+        self.img_grid  = pygame.transform.scale(pygame.image.load('res/images/' + self.THEME + '/grid.png'), (64, 64))
+        self.img_piece = [pygame.transform.scale(pygame.image.load('res/images/' + self.THEME + '/piece_' + str(i) + '.png'), (32, 32)) for i in range(2)]
 
         # một số thông tin về lưới
         # self.grid_width  = 32
@@ -40,16 +41,17 @@ class Game:
         # self.grid_height = (SCREEN_HEIGHT - THICKNESS * NUM_OF_LINES) // (NUM_OF_LINES - 1) + THICKNESS
         self.grid_width   = self.img_grid.get_width() // 2
         self.grid_height  = self.img_grid.get_width() // 2
-        self.grid_start_x = (SCREEN_HEIGHT - self.grid_height * SIZE_Y) // 2
-        self.grid_start_y = (SCREEN_HEIGHT - self.grid_height * SIZE_Y) // 2
-        self.grid_end_x   = self.grid_start_x + self.grid_width  * SIZE_X
-        self.grid_end_y   = self.grid_start_y + self.grid_height * SIZE_Y
+        self.grid_start_x = (self.SCREEN_HEIGHT - self.grid_height * self.SIZE_Y) // 2
+        self.grid_start_y = (self.SCREEN_HEIGHT - self.grid_height * self.SIZE_Y) // 2
+        self.grid_end_x   = self.grid_start_x + self.grid_width  * self.SIZE_X
+        self.grid_end_y   = self.grid_start_y + self.grid_height * self.SIZE_Y
 
         # Khởi tạo Screen
         self.screen = screen
 
         # Screen Win Lose
-        self.win_lose_screen = winlose.WinLose(SCREEN_WIDTH, SCREEN_HEIGHT ,self.screen)
+        print((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        self.win_lose_screen = winlose.WinLose(self.SCREEN_WIDTH, self.SCREEN_HEIGHT ,self.screen)
 
         # các thành phần điều khiển của game
         self.save_manager  = save_manager.SaveManager('game_data.json', 'data')
@@ -57,7 +59,7 @@ class Game:
         self.win_checker   = win_checker.WinChecker()
         self.player_1      = self.game_data['PlayerName']['Player1']
         self.player_2      = self.game_data['PlayerName']['Player2']
-        self.text_switcher = text_switcher.TextSwitcher(self.screen, BACKGROUND_COLOR, [self.player_1, self.player_2])
+        self.text_switcher = text_switcher.TextSwitcher(self.screen, self.BACKGROUND_COLOR, [self.player_1, self.player_2])
         self.cursor_trail  = cursor_trail.CursorTrail()
         self.bot           = bot.Bot()
 
@@ -76,8 +78,8 @@ class Game:
     # tiếp tục game từ save
     def continue_game(self):
         self.draw_grid_on()
-        for row in range(SIZE_X):
-            for column in range(SIZE_Y):
+        for row in range(self.SIZE_X):
+            for column in range(self.SIZE_Y):
                 piece_data = self.game_data['Board'][row][column]
                 # nếu không chứa cờ thì bỏ qua
                 if piece_data == -1:
@@ -93,21 +95,21 @@ class Game:
     def draw_grid_on(self):
 
         # tô screen bằng màu background
-        self.screen.fill(BACKGROUND_COLOR)
+        self.screen.fill(self.BACKGROUND_COLOR)
         # for i in range(self.grid_start_x, SCREEN_WIDTH - self.grid_width * 2, self.grid_width * 2):
         #     for j in range(self.grid_start_y, SCREEN_HEIGHT - self.grid_height * 2, self.grid_height * 2):
         #         self.screen.blit(self.img_grid, (i, j))
 
         cur_y = self.grid_start_y
-        for i in range(SIZE_Y // 2):
+        for i in range(self.SIZE_Y // 2):
             cur_x = self.grid_start_x
-            for j in range(SIZE_X // 2):
+            for j in range(self.SIZE_X // 2):
                 self.screen.blit(self.img_grid, (cur_x, cur_y))
                 cur_x += self.grid_height * 2
             cur_y += self.grid_width * 2
 
         delta = 10
-        pygame.draw.rect(self.screen, LINE_COLOR, (self.grid_start_x - delta, self.grid_start_y - delta, self.grid_end_x - self.grid_start_x + delta * 2, self.grid_end_y - self.grid_start_y + delta * 2), 5, 10)
+        pygame.draw.rect(self.screen, self.LINE_COLOR, (self.grid_start_x - delta, self.grid_start_y - delta, self.grid_end_x - self.grid_start_x + delta * 2, self.grid_end_y - self.grid_start_y + delta * 2), 5, 10)
 
         # # vẽ các ô màu xen kẽ
         # for i in range(0, SCREEN_WIDTH, self.grid_width):
@@ -125,7 +127,7 @@ class Game:
     # vẽ cờ lên màn hình
     def draw_piece_on(self, board_x, board_y, cur_piece):
         # đưa về dạng tâm của bàn cờ
-        (center_x, center_y) = (board_x * self.grid_width + (self.grid_width + THICKNESS) // 2, board_y * self.grid_width + (self.grid_width + THICKNESS) // 2)
+        (center_x, center_y) = (board_x * self.grid_width + (self.grid_width + self.THICKNESS) // 2, board_y * self.grid_width + (self.grid_width + self.THICKNESS) // 2)
 
         # đưa về dạng góc trái trên của cờ trong bàn cờ
         display_pos = (center_x + self.grid_start_x - cur_piece.get_width() / 2, center_y + self.grid_start_y - cur_piece.get_height() / 2)
@@ -151,7 +153,7 @@ class Game:
         self.continue_game()
 
         # nếu đã đánh hết bàn cờ
-        if SIZE_X * SIZE_Y == self.cnt_move:
+        if self.SIZE_X * self.SIZE_Y == self.cnt_move:
             self.end_game = True
 
         # duyệt qua các event
@@ -161,7 +163,7 @@ class Game:
             if gamemode == "Bot":
                 if self.turn == 1:
                     # Comment these lines to enable PvP mode
-                    if SIZE_X * SIZE_Y != self.cnt_move:
+                    if self.SIZE_X * self.SIZE_Y != self.cnt_move:
                         best_move = self.bot.find_best_move(self.game_data['Board'], self.history)
                         cur_piece = self.img_piece[self.game_data['Turn']]
                         self.draw_piece_on(best_move[0], best_move[1], cur_piece)
@@ -198,7 +200,7 @@ class Game:
                     self.text_switcher.switch()
                     
                     # am thanh khi bam chuot
-                    PlaySound.play('res/sounds/click.mp3')
+                    #PlaySound.play('res/sounds/click.mp3')
 
                     # vẽ cờ lên màn hình
                     self.draw_piece_on(board_x, board_y, cur_piece)
@@ -233,7 +235,7 @@ class Game:
         # cập nhật display
         pygame.display.update()
         
-        # delay 120ms
+        # 120 fps
         self.clock.tick(120)
         
         # nếu không có sự kiện gì thì trả về -1
