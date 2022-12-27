c2a1178fc57762eb16e3a49aa3c70b930d1a8205
thucuyenyah <uyenthuc09012004@gmail.com>
2022-12-27 12:02:31 +0700

    them nhac nen, them nhac thang thua,sua duong dan file nhac thang thua

--- newTest/main.py
+++ newTest/main.py
@@ -1,6 +1,6 @@
 import pygame, json, game
 import menu, winlose
-
+from lib.music_game import MusicGame
 # setting
 setting = json.load(open('data/setting.json'))
 SCREEN_WIDTH  = setting['screen']['width']
@@ -9,5 +9,8 @@ SCREEN_HEIGHT = setting['screen']['height']
 if __name__ == "__main__":
     # Khởi tạo Menu
     app = menu.Menu(SCREEN_WIDTH, SCREEN_HEIGHT)
+    
+    MusicGame.play('res/musicgame/musicgame1.mp3')
     # Chạy màn hình game
     app.run()
+
